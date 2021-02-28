66ba47aa24c35f616b4b7ce96f262e06bb7f6923
‘[Jennifer Gonzalez <[kittys201@hotmail.com>
2021-02-28 03:22:37 -0300

    cambio de uso a nasa

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-$(document).ready(function() {
-    var api_key = "563492ad6f91700001000001966bf3f6c7a147e0aded31703f161d99"
-    $("#form").submit(function(event){
-        event.preventDefault()
-        var search = $("#search").val()
+let searchButton = document.querySelector("#search")
 
-        imagesearch()
-    })
-    function imagesearch(){
-        $.ajax({
-            method: 'GET',
-            beforSend: function(xhr){
-                xhr.setRequestHeader ( "Authorization", api_key);
-            },
-            url:"https://api.pexels.com/v1/search?query=" + search + "&per_page=15&page=1"
-            success: function (data) {
-                console.log(data)
-                data.photo.forEach (photo => {
-                  image=`
-                  
-                  <img src="${photo.src.orignal}"width="400" height="300"/>
-                  `
-                  
-                }); 
-            },
-            error: function(eror){
-                 console.log(error)
-            }
-        })
-        
-    }
+
+searchButton.addEventListener("click", ()=>{
+console.log("Boton activado")
+sendApiRequest()
+
 })
+
+async function sendApiRequest() {
+    let api_key = "Y5LxvkWhN3oHZmVejtffuQAf56Nk0eCtpTNAZ99j"
+    let response = await fetch (`https://api.nasa.gov/planetary/apod?api_key=${api_key}`);
+    console.log(response)
+    let data = await response.json()
+    console.log(data)
+    useApiData(data)
+}
+function useApiData(data){
+
+    document.querySelector("#content").innerHTML += data.explanation
+    document.querySelector("#content").innerHTML += `<img src="${data.url}">`
+}
